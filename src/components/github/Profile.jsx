75953f32fa4a6a68619a3ea3e93931cd2847b8da
shafi <mshafivk@gmail.com>
2016-05-18 00:55:35 +0800

    [Shafi]Removed importing reactDOM in git components

--- src/components/github/Profile.jsx
+++ src/components/github/Profile.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import RepoList from './RepoList.jsx';
 class Profile extends Component {
 	render () {
--- src/components/github/RepoList.jsx
+++ src/components/github/RepoList.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import Repo from './Repo.jsx';
 class RepoList extends Component {
 	render () {
